24a6788dfad382ffe7d9874e3db20a8b7f2f322b
Franco Coll <raku3456@gmail.com>
2024-12-10 16:32:11 -0300

    asdas

--- wait-for-sql.sh
+++ wait-for-sql.sh
@@ -1,18 +1,3 @@
-#!/bin/sh
-
-# Espera hasta que el servidor MySQL esté listo
-timeout=600 # Aumenta el tiempo de espera a 600 segundos (10 minutos)
-count=0
-
-while ! nc -z mysql.railway.internal 3306; do
-  echo "Esperando a que MySQL esté listo..."
-  sleep 2
-  count=$((count + 2))
-  if [ $count -ge $timeout ]; then
-    echo "El tiempo de espera para MySQL ha expirado."
-    exit 1
-  fi
-done
 
 echo "MySQL está listo. Aplicando migraciones de Prisma..."
 npx prisma migrate deploy
